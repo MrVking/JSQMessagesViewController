5e75b765e20cd9b60d96c75c0673460cbc0f3d6e
saadjulien <julien.saad@polymtl.ca>
2015-08-31 15:46:25 -0400

    Update sound player

--- JSQMessagesDemo/DemoModelData.m
+++ JSQMessagesDemo/DemoModelData.m
@@ -18,8 +18,6 @@
 
 #import "DemoModelData.h"
 
-#import "NSUserDefaults+DemoSettings.h"
-
 /**
  *  This is for demo/testing purposes only.
  *  This object sets up some fake model data.
@@ -48,27 +46,27 @@
          *
          *  If you are not using avatars, ignore this.
          */
-//        JSQMessagesAvatarImage *jsqImage = [JSQMessagesAvatarImageFactory avatarImageWithUserInitials:@"JSQ"
-//                                                                                      backgroundColor:[UIColor colorWithWhite:0.85f alpha:1.0f]
-//                                                                                            textColor:[UIColor colorWithWhite:0.60f alpha:1.0f]
-//                                                                                                 font:[UIFont systemFontOfSize:14.0f]
-//                                                                                             diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
-//        
-//        JSQMessagesAvatarImage *cookImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_cook"]
-//                                                                                       diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
-//        
-//        JSQMessagesAvatarImage *jobsImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_jobs"]
-//                                                                                       diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
-//        
-//        JSQMessagesAvatarImage *wozImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_woz"]
-//                                                                                      diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
-//        
-//        self.avatars = @{ kJSQDemoAvatarIdSquires : jsqImage,
-//                          kJSQDemoAvatarIdCook : cookImage,
-//                          kJSQDemoAvatarIdJobs : jobsImage,
-//                          kJSQDemoAvatarIdWoz : wozImage };
-//        
-//        
+        JSQMessagesAvatarImage *jsqImage = [JSQMessagesAvatarImageFactory avatarImageWithUserInitials:@"JSQ"
+                                                                                      backgroundColor:[UIColor colorWithWhite:0.85f alpha:1.0f]
+                                                                                            textColor:[UIColor colorWithWhite:0.60f alpha:1.0f]
+                                                                                                 font:[UIFont systemFontOfSize:14.0f]
+                                                                                             diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
+        
+        JSQMessagesAvatarImage *cookImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_cook"]
+                                                                                       diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
+        
+        JSQMessagesAvatarImage *jobsImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_jobs"]
+                                                                                       diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
+        
+        JSQMessagesAvatarImage *wozImage = [JSQMessagesAvatarImageFactory avatarImageWithImage:[UIImage imageNamed:@"demo_avatar_woz"]
+                                                                                      diameter:kJSQMessagesCollectionViewAvatarSizeDefault];
+        
+        self.avatars = @{ kJSQDemoAvatarIdSquires : jsqImage,
+                          kJSQDemoAvatarIdCook : cookImage,
+                          kJSQDemoAvatarIdJobs : jobsImage,
+                          kJSQDemoAvatarIdWoz : wozImage };
+        
+        
         self.users = @{ kJSQDemoAvatarIdJobs : kJSQDemoAvatarDisplayNameJobs,
                         kJSQDemoAvatarIdCook : kJSQDemoAvatarDisplayNameCook,
                         kJSQDemoAvatarIdWoz : kJSQDemoAvatarDisplayNameWoz,
@@ -108,25 +106,6 @@
                                                      date:[NSDate distantPast]
                                                      text:@"It is simple, elegant, and easy to use. There are super sweet default settings, but you can customize like crazy."],
                      
-                     [[JSQMessage alloc] initWithSenderId:kJSQDemoAvatarIdSquires
-                                        senderDisplayName:kJSQDemoAvatarDisplayNameSquires
-                                                     date:[NSDate distantPast]
-                                                     text:@"It even has data detectors. You can call me tonight. My cell number is 123-456-7890. My website is www.hexedbits.com."],
-                     
-                     [[JSQMessage alloc] initWithSenderId:kJSQDemoAvatarIdJobs
-                                        senderDisplayName:kJSQDemoAvatarDisplayNameJobs
-                                                     date:[NSDate date]
-                                                     text:@"JSQMessagesViewController is nearly an exact replica of the iOS Messages App. And perhaps, better."],
-//                     
-//                     [[JSQMessage alloc] initWithSenderId:kJSQDemoAvatarIdCook
-//                                        senderDisplayName:kJSQDemoAvatarDisplayNameCook
-//                                                     date:[NSDate date]
-//                                                     text:@"It is unit-tested, free, open-source, and documented."],
-//                     
-//                     [[JSQMessage alloc] initWithSenderId:kJSQDemoAvatarIdSquires
-//                                        senderDisplayName:kJSQDemoAvatarDisplayNameSquires
-//                                                     date:[NSDate date]
-//                                                     text:@"Now with media messages!"],
                      nil];
     
     // [self addPhotoMediaMessage];
